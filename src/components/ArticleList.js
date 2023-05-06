b6e1b4fb38c6115fb81f5bf88c7210c8835cbaa6
Jackline-ke <antoninahodhiambo@gmail.com>
2023-05-06 05:23:28 +0300

    Completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,18 @@
+// @flow strict
+
+import * as React from 'react';
+import Article from './Article';
+
+function ArticleList({ posts }) {
+    const myPosts = posts.map((post) => {
+        return (
+            <Article key={post.id} title={post.title} date={post.date} preview={post.preview} />
+        )
+    })
+    return (
+        <main>
+            {myPosts}
+      </main>
+      )
+    }
+export default ArticleList;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// @flow strict
+
+import * as React from 'react';
+
+function Header(name) {
+    return (
+        <div>
+            <h1>{name}</h1>
+        </div>
+    );
+};
+
+export default Header;
